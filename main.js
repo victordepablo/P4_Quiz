4a18871b64172c714cd7cc32c533fb8e89a3b46c
victordepablo <victor.depablo.97@gmail.com>
2018-02-28 12:48:13 +0100

    P2 Quiz funciones quizzes

--- main.js
+++ main.js
@@ -51,6 +51,119 @@ const errorlog = (emsg) => {
     console.log(`${colorize("Error", "red")}: ${colorize(colorize(emsg, "red"), "bgYellowBright")}`);
 };
 
+//Modelo de datos
+//
+// Enesta variable se mantienen todos los quizzes existentes.
+// Es un array de objetos, donde cada objeto tiene los atributos question
+// y answer para guardar el texto de la pregunta y el de la respuesta.
+let quizzes = [
+    {
+        question: "Capital de Italia",
+        answer: "Roma"
+    },
+    {
+        question: "Capital de Francia",
+        answer: "París"
+    },
+    {
+        question: "Capital de España",
+        answer: "Madrid"
+    },
+    {
+        question: "Capital de Portugal",
+        answer: "Lisboa"
+    }
+    ];
+
+//
+/**
+ * Devuelve el numero total de preguntas existentes.
+ *
+ * @return {number} numero total de preguntas existentes.
+ */
+const count = () => quizzes.lenght;
+
+
+/**
+ * Añade un nuevo quiz
+ *
+ * @param question String con la pregunta.
+ * @param answer String con la respuesta
+ */
+const add = (question, answer) => {
+    quizzes.push({
+        question: (question || "").trim(),
+        answer: (answer || "").trim()
+    });
+};
+
+
+/**
+ * Actualiza el quiz situado en la posición index
+ *
+ * @param id Clave que identifica el quiz a actualizar.
+ * @param question String con la pregunta.
+ * @param answer String con la respuesta.
+ */
+const update = (id, question, answer) => {
+    const quiz = quizzes[id];
+    if (typeof quiz === "undefined"){
+        throw new Error(`El valor del parametro id no es valido.`);
+    }
+    quizzes.splice(id, 1, {
+            question: (question || "").trim(),
+            answer: (answer || "").trim()
+    });
+};
+
+
+/**
+ * Devuelve todos los quizzes existentes.
+ *
+ * Devuelve un clon del valor guardado en la variable quizzes, es decir devuelve un
+ * objeto nuevo con todas las preguntas existentes
+ * Para clonar quizzes se usa stringify + parse.
+ *
+ * @returns {any}
+ */
+const getAll = () => JSON.parse(JSON.stringify(quizzes));
+
+
+/**
+ * Devuelve un clon del quiz almacenado en la posición dada.
+ *
+ * Para clonar el quiz se usa stringify + parse.
+ *
+ * @param id Clave que identifica el quiz a devolver.
+ *
+ * @returns {question, answer} Devuelve el objeto quiz de la posición dada
+ */
+const getByIndex = id => {
+
+    const quiz = quizzes[id];
+    if (typeof quiz === "undefined") {
+        throw new Error(`El valor del parámetro id no es válido.`);
+    }
+    return JSON.parse(JSON.stringify(quiz));
+};
+
+
+//
+/**
+ * Elimina el quiz situado en la posición dada.
+ *
+ * @param id Clave que identifica el quiz a borrar..
+ */
+const deleteByIndex = id => {
+
+    const quiz = quizzes[id];
+    if (typeof quiz === "undefined") {
+        throw new Error(`El valor del parámetro id no es válido.`);
+    }
+    quizzes.splice(id, 1);
+};
+
+
 // Mensaje inicial
 biglog ('CORE Quiz', 'green');
 
